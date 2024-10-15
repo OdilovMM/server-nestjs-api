fd77427bb0d9ac46e75432099657e16af3bdf585
OdilovMM <dssrinman@gmail.com>
2024-10-15 23:44:50 +0500

    add: created components

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
+import { CommentModule } from './comment/comment.module';
 config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URL)],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), UserModule, PostModule, CategoryModule, TagModule, CommentModule],
   controllers: [],
   providers: [],
 })
